d6c114a04fdfa0e721191a198b852517080a7f6f
juicyfruit2 <134799673+juicyfruit2@users.noreply.github.com>
2023-07-06 15:32:45 +0200

    Add files via upload

---
+++ harsh.py
@@ -0,0 +1,5 @@
+# created a dictionary that groups together countries and their capitals 
+
+countryMap = {"Brazil":"Rio de jinerio", "France":"Paris" , "Italy":"Rome"}
+
+print(countryMap["Brazil"])
